af8a1a046618ae3ff916913ba4afabbd726b1695
derrickturk <dwt@terminusdatascience.com>
2017-02-08 13:40:23 -0600

    UI for hikari.

--- hikeshi.py
+++ hikeshi.py
@@ -1,3 +1,17 @@
+# hikeshi
+# dump data functions from an existing project in a format tobikuchi can read
+# (c) 2017 dwt | terminus data science, LLC
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import clr
+clr.AddReference('System.Windows.Forms')
+
+from System.IO import Path
+from System.Windows.Forms import FolderBrowserDialog, DialogResult
+
 from Spotfire.Dxp.Data.DataFunctions import *
 
 def dump_function(df, stream):
@@ -39,10 +53,35 @@ def dump_output(op, stream):
             stream.write('# TK_DESC %s\n' % line)
 
 def dump_script(builder, stream):
-    stream.write(builder.Settings['script'])
+    stream.write(builder.Settings['script'].replace('\r', ''))
     if not builder.Settings['script'].endswith('\n'):
         stream.write('\n')
 
+def get_output_directory():
+    folder_dialog = FolderBrowserDialog()
+    folder_dialog.ShowNewFolderButton = True
+    folder_dialog.Description = 'Output directory for R scripts'
+    if folder_dialog.ShowDialog() != DialogResult.OK:
+        return None
+    return folder_dialog.SelectedPath
+
+def make_filename(df):
+    return df.Name + '.R'
+
+def report_error(fn, err):
+    MessageBox.Show('Error processing %s:\n%s' % (fn, err),
+            'tobikuchi error', MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+# spotfire "main"
 import sys
-for df in Document.Data.DataFunctions:
-    dump_function(df, sys.stdout)
+output_dir = get_output_directory()
+if output_dir is not None:
+    for df in Document.Data.DataFunctions:
+        fn = make_filename(df)
+        try:
+            f = open(Path.Combine(output_dir, fn), 'w')
+            dump_function(df, f)
+        except Exception, e:
+            report_error(fn.Name, e)
+        finally:
+            f.close()
